e5bdb72944510f0326df174fc0a0737aa7cbb125
kingjethru <54653382+kingjethru@users.noreply.github.com>
2022-06-12 21:33:06 +0800

    email data

--- src/app/component/issuance/coverage/coverage.component.ts
+++ src/app/component/issuance/coverage/coverage.component.ts
@@ -24,7 +24,7 @@ export class CoverageComponent implements OnInit {
 
   submit() {
     const policyNumber = this.referenceNumber;
-    this.issuanceService.sendEmail(emailData).then((result: ReturnDTO) => {
+    this.issuanceService.sendEmail(this.emailData).then((result: ReturnDTO) => {
       if (result.status) {
         Swal.fire({
           title: 'Thank you for submiting your application',
--- src/app/objects/emailData.ts
+++ src/app/objects/emailData.ts
@@ -4,6 +4,5 @@ export class emailData {
   contactNumber: string;
   clientName: string;
   
-  constructor() {
-  }
+  constructor() {}
 }
